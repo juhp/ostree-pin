2750d03da4c2aecc345f349a231eee6b156cd79a
Jens Petersen <petersen@redhat.com>
2021-09-28 16:47:35 +0800

    initial poc
    rust port of Haskell code

---
+++ src/main.rs
@@ -0,0 +1,18 @@
+fn main() {
+    let sysroot = &ostree::Sysroot::new_default();
+    sysroot
+        .load(gio::NONE_CANCELLABLE)
+        .expect("failed to load sysroot");
+    let booted = sysroot.booted_deployment().expect("no booted deployment!");
+    if !(booted.is_pinned()) {
+        sysroot
+            .deployments()
+            .iter_mut()
+            .filter(|d| d.is_pinned())
+            .try_for_each(|pin| sysroot.deployment_set_pinned(pin, false))
+            .expect("failed to unpin deployments");
+    }
+    sysroot
+        .deployment_set_pinned(&booted, true)
+        .expect("failed to pin booted");
+}
