afaa879b73b6d4602966c3fd83445e5159909771
Jens Petersen <juhpetersen@gmail.com>
2023-10-16 22:56:44 +0800

    add --help --dryrun and --rollback options
    using simple-cmd-args

--- Main.hs
+++ Main.hs
@@ -2,31 +2,48 @@
 
 module Main (main) where
 
---import BasicPrelude
-
 import Control.Monad
+import Data.Maybe (isNothing)
 import GI.Gio.Objects.Cancellable
 import GI.OSTree
-import System.Environment (getArgs)
+import SimpleCmdArgs
 
 main :: IO ()
-main = do
-  args <- getArgs
-  unless (null args) $
-    error "Usage: without arguments to pin current deployment"
-  sysroot <- sysrootNewDefault
-  sysrootLoad sysroot (Nothing @Cancellable)
-  mbooted <- sysrootGetBootedDeployment sysroot
-  case mbooted of
-    Nothing -> error "No booted deployment found!"
-    Just booted -> do
-      pinned <- deploymentIsPinned booted
-      if pinned then return ()
+main =
+  simpleCmdArgs Nothing "Tool to update pinned ostree deployment"
+  "See https://github.com/juhp/ostree-pin#readme" $
+  program
+  <$> switchWith 'n' "dryrun" "Dry run"
+  <*> switchWith 'p' "rollback" "Pin previous (rollback) deployment"
+  where
+    -- FIXME allow deployment# ?
+    program dryrun rollback = do
+      sysroot <- sysrootNewDefault
+      sysrootLoad sysroot (Nothing @Cancellable)
+      mdeploy <-
+        if rollback
+        then do
+          (_,mprevious) <- sysrootQueryDeploymentsFor sysroot Nothing
+          return mprevious
         else do
-        deploys <- sysrootGetDeployments sysroot
-        pins <- filterM deploymentIsPinned deploys
-        case pins of
-          [] -> return ()
-          [pin] -> sysrootDeploymentSetPinned sysroot pin False
-          _ -> putStrLn "more than one deployment already pinned"
-      sysrootDeploymentSetPinned sysroot booted True
+          mbooted <- sysrootGetBootedDeployment sysroot
+          if isNothing mbooted
+            then error "No booted deployment found!"
+            else return mbooted
+      case mdeploy of
+        Nothing -> error "deployment not found"
+        Just deploy -> do
+          pinned <- deploymentIsPinned deploy
+          if pinned
+            then return ()
+            else do
+            deploys <- sysrootGetDeployments sysroot
+            pins <- filterM deploymentIsPinned deploys
+            case pins of
+              [] -> return ()
+              [pin] ->
+                unless dryrun $
+                sysrootDeploymentSetPinned sysroot pin False
+              _ -> putStrLn "more than one deployment already pinned"
+          unless dryrun $
+            sysrootDeploymentSetPinned sysroot deploy True
